d331e051e813df42fcc30c20201fcdf2a267e2c8
Renier Vargas <renier1989@gmail.com>
2023-11-05 18:27:25 -0500

    319. Añadiendo más Información al Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,24 @@
 import useCotizador from "../hooks/useCotizador";
-
+import {MARCAS, PLANES} from "../constants"
 const Resultado = () => {
-  const { resultado } = useCotizador();
-    if(resultado === 0){
-        return
-    }
-  return <div>{resultado}</div>;
+  const { resultado,datos } = useCotizador();
+  const {marca, plan, year} = datos
+
+  const [nombreMarca] = MARCAS.filter(m =>m.id === Number(marca));
+  console.log(nombreMarca);
+
+  if (resultado === 0) {
+    return;
+  }
+  return <div className="bg-gray-100 text-center p-5 mt-5 shadow">
+    <h2 className="uppercase text-gray-600 font-bold text-3xl">
+            Resumen
+    </h2>
+    <p className="my-2"> 
+    <span className="font-bold">Marca : </span>
+        {marca}
+    </p>
+  </div>;
 };
 
 export default Resultado;
