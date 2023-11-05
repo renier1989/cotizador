8c8e6d3512ff4ce262799bca11d19092b3771b6b
Renier Vargas <renier1989@gmail.com>
2023-11-05 17:50:32 -0500

    316. Trabajando con el Algoritmo de Cotización (parte 2 de 2)

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import { createContext } from "react";
-import { obtnerDiferenciaYear } from "../helpers";
+import { calcularMarca, calcularPlan, formatoDinero, obtnerDiferenciaYear } from "../helpers";
 
 const CotizadorContext = createContext();
 
@@ -13,6 +13,7 @@ const CotizadorProvider = ({ children }) => {
         year: "",
         plan: "",
     });
+    const [resultado , setResultado] = useState(0);
 
     const handleChange = e => {
         setDatos({
@@ -28,19 +29,28 @@ const CotizadorProvider = ({ children }) => {
         let resultado = 2000;
         // luego se obtiene la diferencia de los años 
         let diferencia = obtnerDiferenciaYear(datos.year);
-        console.log('diferencia', diferencia );
-        resultado -= ((diferencia * 3) * resultado) / 100;
+        // console.log('diferencia AÑOS: ', diferencia );
         // se resta el 3% por cada año
-        console.log(resultado);
+        resultado -= ((diferencia * 3) * resultado) / 100;
+        // console.log('resta del 3%:', resultado);
 
-        // Amaricano 15%
         // Europeo 30%
-        // Asiatico 5%
-        // Ruso 10%
+        // Amaricano 15%
+        // Asiatico 7%
+        // Ruso 3%
+        resultado *= calcularMarca(datos.marca);
+        // console.log('incremento por marca: ', resultado);
 
         // Plan Basico 20%
         // Plan Completo 50%
+        resultado *= calcularPlan(datos.plan);
+        // console.log('incremento por plan: ', resultado);
+
+        resultado = formatoDinero(resultado);
+        // console.log('final:', resultado);
+        setResultado(resultado);
     }
+
 
   return (
     <CotizadorContext.Provider
@@ -50,6 +60,8 @@ const CotizadorProvider = ({ children }) => {
         error,
         setError,
         cotizarSeguro,
+        resultado,
+        setResultado,
       }}
     >
       {children}
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,38 @@
 export const obtnerDiferenciaYear = year =>{
     return new Date().getFullYear() - year;
 }
+
+export function calcularMarca(marca){
+    let incremento
+
+    switch (marca) {
+        case '1':
+            incremento = 1.30;
+            break;
+        case '2':
+            incremento = 1.15;
+            break;
+        case '3':
+            incremento = 1.07;
+            break;
+        case '4':
+            incremento = 1.03;
+            break;
+    
+        default:
+            break;
+    }
+
+    return incremento;
+}
+
+export function calcularPlan(plan){
+    return plan === '1' ? 1.20 : 1.50;
+}
+
+export function formatoDinero(cantidad){
+    return cantidad.toLocaleString('en-US', {
+        style: 'currency',
+        currency : 'USD',
+    })
+}
